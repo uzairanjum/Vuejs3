fcada96649113ac9c770f33e2e67660bcde892ac
uzairanjum <rajpoot.waryah@gmail.com>
2021-03-09 16:22:55 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+
+ <h2> Parent Component </h2>
+ <h3>{{title}}</h3><hr>
+ <input type="text" ref="name">
+ <button @click="clickfunction">Click here</button><hr>
+
+ <div v-if="showmodel">
+   <Child  :header="header" :footer="footer" @close="show" >
+   <hr><hr><h3>template sharing from parent to child using slot</h3><hr><hr>
+   <h3>specifically mention slot </h3>
+   <template v-slot:links>
+     <a href="#">signup now </a>
+   </template>
+   </Child>
+    </div>
+
+    <h3>Using $emits (parent to child) </h3>
+    <button @click="show">Showchild</button><hr><hr> 
+ 
+ 
+
+ 
+</template>
+
+<script>
+import Child from './components/Child.vue'
+export default {
+  name: 'App',
+  components: { Child },
+
+  data(){
+    return{
+      title:"Access Text field by using Template $ref",
+      header:"1st prop",
+      footer:"2nd prop",
+      showmodel:false
+    }
+  },
+   methods:{
+    clickfunction(){      
+      console.log(this.$refs.name)
+      
+      this.$refs.name.focus()
+    },
+    show(){
+      this.showmodel =! this.showmodel
+    }
+  }
+    
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="backdrop" @click.self="closechild">
+        <div class="modal">
+            <h1>{{ header }}</h1>
+            <h1>{{ footer }}</h1>
+            <slot></slot>
+            <div>
+                <slot name="links"></slot><hr><hr>
+            </div>
+            
+
+            
+            
+            <p>------ Child Components -----</p>
+
+        </div> 
+    </div>
+</template>
+<script>
+export default {
+    props:['header' ,'footer'],
+    methods:{
+         closechild(){
+           this.$emit('close')
+
+    }
+
+   
+
+    }
+}
+</script>
+
+
+<style>
+.modal{
+    width:400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgb(0, 0, 0 ,.5);
+    width: 100%;
+    height: 100%;
+}
+
+</style>
